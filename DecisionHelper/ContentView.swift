c196d81b7583bee2dc573e95bab63426b1a2490b
GuanGuanGJo <abcz86108@gmail.com>
2025-12-24 10:55:25 +0800

    Initial Commit

---
+++ DecisionHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DecisionHelper
+//
+//  Created by 關關的m4 macbook pro on 2025/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
